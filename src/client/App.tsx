76d8e42929f9187019e0edcf7231f169188f90ef
Angel D. Munoz <scythes.lol@gmail.com>
2024-02-07 21:47:49 -0600

    add todos example to the app

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 import reactLogo from "./assets/react.svg";
+import { Todos } from "./Todos";
 
 type AppStatus = "loading" | "idle" | ["error", string];
 
@@ -59,6 +60,7 @@ function App() {
         <p className="read-the-docs">
           Click on the Vite and React logos to learn more
         </p>
+        <Todos />
       </div>
     </div>
   );
